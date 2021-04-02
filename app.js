cea847ae904566d76a4c9cbb76ab584335e11d94
alector <panagiotispp@gmail.com>
2021-04-02 19:50:11 +0200

    change variables

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+const content = ` <h1> Hello world!</h1>
+<p> This is a simple application made with express </p>
+`
 app.get("/", (req, res) => {
-  res.send("Hello World!")
+  res.send(content)
 })
 
 app.listen(port, () => {
