fae7857bc1b49acb71612c992aa2171fc211c642
alector <panagiotispp@gmail.com>
2021-04-02 21:43:09 +0200

    minor detail

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-const content = ` <h1> Hello world!</h1>
+let content = ` <h1> Hello world!</h1>
 <p> This is a simple application made with express </p>
 `
 app.get("/", (req, res) => {
